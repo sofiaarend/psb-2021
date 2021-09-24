6cc40e65aefad355f85651cd744b08dc45d87709
sofiaarend21 <sofiaarend@gaudium.com.br>
2021-09-24 01:15:21 -0300

    codigo ex3

--- ex3-PSB.c
+++ ex3-PSB.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello World");
+struct person {
+    char *name;
+    char *nation;
+    char *birth;
+    char *pic;
+};
+
+void setPersonsStruct(FILE *file, struct person *p[10]) {
+    int i = 0;
+    char line[100];
+
+    while (fgets(line, sizeof(line), file)) {
+        sscanf(line, "%s %s %s %s", p[i]->name, p[i]->nation, p[i]->birth, p[i]->pic);
+        i++;
+    }
+}
+
+void getNextBirth(struct person *p[10], char *next) {
+    int m, dif = 0;
+    for (int i = 0; i < 10; ++i) {
+        dif += labs(p[i]->birth - p[i+1]->birth);
+    }
+    m = dif/10;
+    next = p[9]->birth + m;
+}
+
+
+int main() {
+    struct person *p[10];
+    char next;
+
+    FILE *file = fopen("/aux/personalidades.txt", "r");
+    if (file) {
+        setPersonsStruct(file, p);
+        fclose(file);
+    } else {
+        printf("Couldn't open file\n");
+        return 1;
+    }
+
+    getNextBirth(p, &next);
+    printf("The next year of birth is: %d\n", next);
 
     return 0;
 }
