fefe6929f100ca604bda951be21b02076737b17e
sofiaarend21 <sofiaarend@gaudium.com.br>
2021-09-27 21:33:48 -0300

    correções

--- ex3-PSB.c
+++ ex3-PSB.c
@@ -2,47 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct person {
-    char *name;
-    char *nation;
-    char *birth;
-    char *pic;
-};
+#define NAME_LEN 29
+#define MAX_PARTS 10
 
-void setPersonsStruct(FILE *file, struct person *p[10]) {
+struct person {
+    char name[NAME_LEN+1];
+    char nation[NAME_LEN+1];
+    int birth;
+    char pic[NAME_LEN+1];
+} p[MAX_PARTS];
+
+void setPersonsStruct(FILE *file) {
     int i = 0;
     char line[100];
 
     while (fgets(line, sizeof(line), file)) {
-        sscanf(line, "%s %s %s %s", p[i]->name, p[i]->nation, p[i]->birth, p[i]->pic);
+        sscanf(line, "%s %s %d %s", &p[i].name, &p[i].nation, &p[i].birth, &p[i].pic);
         i++;
     }
 }
 
-void getNextBirth(struct person *p[10], char *next) {
+void getNextBirth(int *next) {
     int m, dif = 0;
-    for (int i = 0; i < 10; ++i) {
-        dif += labs(p[i]->birth - p[i+1]->birth);
+    for (int i = 0; i < 9; ++i) {
+        dif += abs(p[i].birth - p[i+1].birth);
     }
     m = dif/10;
-    next = p[9]->birth + m;
+    *next = p[9].birth + m;
 }
 
 
 int main() {
-    struct person *p[10];
-    char next;
+    int next;
 
-    FILE *file = fopen("/aux/personalidades.txt", "r");
+    FILE *file = fopen("/uploads/personalidades.txt", "r");
     if (file) {
-        setPersonsStruct(file, p);
+        setPersonsStruct(file);
         fclose(file);
     } else {
         printf("Couldn't open file\n");
         return 1;
     }
 
-    getNextBirth(p, &next);
+    getNextBirth(&next);
     printf("The next year of birth is: %d\n", next);
 
     return 0;
